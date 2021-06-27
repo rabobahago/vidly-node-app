ed9de43946920bd752cc7bcf060f565741fded0c
rabobahago <rabobahago@gmail.com>
2021-06-27 15:42:17 +0100

    customers route added

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-const express = require("express");
-const routes = require("./routes/genres");
-const allMovies = require("./allMovies/allMovies");
 const mongoose = require("mongoose");
+const genres = require("./routes/genres");
+const customers = require("./routes/customers");
+const express = require("express");
 const app = express();
 app.use(express.json());
-app.use("/api/genres", routes);
-app.use("/", allMovies);
+app.use("/api/genres", genres);
+app.use("/api/customers", customers);
 mongoose
   .connect("mongodb://localhost/vidly")
-  .then(() => {
-    console.log("connect to mongodb....");
-  })
-  .catch((er) => {
-    console.log(er.message);
-  });
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB..."));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(port, () => console.log(`Listening on port ${port}...`));
--- routes/customers.js
+++ routes/customers.js
@@ -1,16 +1,15 @@
-const Joi = require("joi");
-const mongoose = require("mongoose");
 const express = require("express");
+const Joi = require("joi");
 const router = express.Router();
-
+const mongoose = require("mongoose");
 const Customer = mongoose.model(
-  "Customer",
+  "Customers",
   new mongoose.Schema({
     name: {
       type: String,
       required: true,
-      minlength: 5,
-      maxlength: 50,
+      minLength: 5,
+      maxLength: 50,
     },
     isGold: {
       type: Boolean,
@@ -19,73 +18,56 @@ const Customer = mongoose.model(
     phone: {
       type: String,
       required: true,
-      minlength: 5,
-      maxlength: 50,
+      minLength: 5,
+      maxLength: 50,
     },
   })
 );
 
 router.get("/", async (req, res) => {
   const customers = await Customer.find().sort("name");
-  res.send(customers);
+  res.status(200).send(customers);
 });
 
 router.post("/", async (req, res) => {
   const { error } = validateCustomer(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-
-  let customer = new Customer({
+  let customer = Customer({
     name: req.body.name,
-    isGold: req.body.isGold,
     phone: req.body.phone,
+    isGold: req.body.isGold,
   });
   customer = await customer.save();
-
-  res.send(customer);
+  return res.status(200).send(customer);
 });
 
 router.put("/:id", async (req, res) => {
   const { error } = validateCustomer(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-
-  const customer = await Customer.findByIdAndUpdate(
+  let customer = await Customer.findByIdAndUpdate(
     req.params.id,
-    {
-      name: req.body.name,
-      isGold: req.body.isGold,
-      phone: req.body.phone,
-    },
+    { name: req.body.name, phone: req.body.phone, isGold: req.body.isGold },
     { new: true }
   );
-
   if (!customer)
-    return res
-      .status(404)
-      .send("The customer with the given ID was not found.");
+    return res.status(404).send("Genre with the given ID not found");
 
-  res.send(customer);
+  return res.status(200).send(customer);
 });
-
 router.delete("/:id", async (req, res) => {
-  const customer = await Customer.findByIdAndRemove(req.params.id);
-
+  let customer = await Customer.findByIdAndRemove(req.params.id);
   if (!customer)
-    return res
-      .status(404)
-      .send("The customer with the given ID was not found.");
-
-  res.send(customer);
+    return res.status(404).send("Genre with the given ID not found");
+  return res.status(200).send(customer);
 });
 
 router.get("/:id", async (req, res) => {
   const customer = await Customer.findById(req.params.id);
-
-  if (!customer)
-    return res
-      .status(404)
-      .send("The customer with the given ID was not found.");
-
-  res.send(customer);
+  if (!customer) {
+    res.status(404).send("Genre with the given ID not found");
+    return;
+  }
+  return res.status(200).send(customer);
 });
 
 function validateCustomer(customer) {
@@ -94,7 +76,6 @@ function validateCustomer(customer) {
     phone: Joi.string().min(5).max(50).required(),
     isGold: Joi.boolean(),
   };
-
   return Joi.validate(customer, schema);
 }
 
